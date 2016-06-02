5c5fbcbc53a0a951c98193be3155dfba0120c767
bachino90 <emilianobivachi@gmail.com>
2016-06-02 12:23:11 -0300

    Initial Commit

---
+++ Recipes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by Restorando on 6/2/16.
+//  Copyright © 2016 Restorando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
