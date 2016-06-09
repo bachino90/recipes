e04836ba7469e8f0808c0fd96e2ed4d1a7103953
bachino90 <emilianobivachi@gmail.com>
2016-06-09 16:37:22 -0300

    Fix

--- Recipes/AppDelegate.swift
+++ Recipes/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        let navController = UINavigationController(rootViewController: RecipeTableViewController())
+        let navController = UINavigationController(rootViewController: RecipeSectionsViewController())
         window!.rootViewController = navController
         window!.makeKeyAndVisible()
         return true
